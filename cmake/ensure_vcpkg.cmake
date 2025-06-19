e3fa49437a28385ff4036bb518c251b27b8d96a0
John Manard III <johnmanardiii@gmail.com>
2025-06-19 15:36:33 -0700

    Happy enough with this cmake project as a starting point.

---
+++ cmake/ensure_vcpkg.cmake
@@ -0,0 +1,23 @@
+function (ensure_vcpkg VCPKG_ROOT)
+    # Set VCPKG paths depdening on OS
+    
+    if(WIN32)
+        set(VCPKG_EXECUTABLE "${VCPKG_ROOT}/vcpkg")
+        set(VCPKG_BOOTSTRAP_SCRIPT "${VCPKG_ROOT}/bootstrap-vcpkg.bat")
+    else()
+        message(FATAL_ERROR "Project only configured for Windows! Exiting...")
+    endif()
+
+    # Run the bootstrap script if the executable doesn't exist
+    if(NOT EXISTS ${VCPKG_EXECUTABLE})
+        message(STATUS "Running vcpkg bootstrap script")
+        execute_process(COMMAND ${VCPKG_BOOTSTRAP_SCRIPT}
+                        WORKING_DIRECTORY ${VCPKG_ROOT}
+                        RESULT_VARIABLE VCPKG_BOOTSTRAP_RESULT)
+        if(NOT VCPKG_BOOTSTRAP_RESULT EQUAL 0)
+            message(FATAL_ERROR "vcpkg bootstrap failed, exiting...")
+        else()
+            message(STATUS "vcpkg bootstrap complete")
+        endif()
+    endif()
+endfunction()
--- scripts/linux-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# TODO: CREATE A BRANCHING SCRIPT THAT HANDLES WINDOWS, also this is ubuntu only D:
-sudo apt update
-sudo apt install -y build-essential libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config zip ninja-build libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config
